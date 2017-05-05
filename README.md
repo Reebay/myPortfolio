0b04b0fa6c6cb0bc1d4d71f8a8170fccb4bad926
Reebay <reebay@mail.ru>
2017-05-05 11:26:04 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# myPortfolio
+# Сайт портфолио
+
+[![N|Solid](https://s8.hostingkartinok.com/uploads/images/2017/05/86c171b0f49946056950e287a75de758.jpg)](https://github.com/Reebay/myPortfolio)
---
+++ app/js/common.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+
+	$("#my-menu").mmenu({
+		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
+         navbar: {
+        	title: '<img src="./img/logo.png" alt="Лого навигации">'
+         },
+         offCanvas: {
+         	position: 'right'
+         }
+       
+	});
+
+
+	var api = $('#my-menu').data('mmenu');
+
+	api.bind('open:finish', function() {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+
+
+	$('.carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function(){
+			carouselService()
+		}, 100);
+	});
+
+	$('.carousel-services').owlCarousel({
+		// loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}
+	});
+
+
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths = $(this),
+				thsh = ths.find('.carousel-services-content').outerHeight();
+				ths.find('.carousel-services-image').css('min-height', thsh)
+		});
+	}carouselService();
+
+	$('.carousel-services-composition .h3').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+	$('section .h2').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+	});
+
+	$('.reviews').owlCarousel({
+		loop: true,
+		items: 1,
+		smartSpeed: 700,
+		nav: false
+	});
+
+	$('form.callback').submit(function() { //Change
+			var th = $(this);
+			$.ajax({
+				type: "POST",
+				url: "mail.php", //Change
+				data: th.serialize()
+			}).done(function() {
+				$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+				setTimeout(function() {
+					$(th).find('.success').removeClass('active').fadeOut();
+					th.trigger("reset");
+				}, 3000);
+			});
+			return false;
+	});
+
+	//Resize Window
+	function onResize() {
+		$('.carousel-services-content').equalHeights();
+	}
+	window.onresize = function() {onResize()};
+
+});
